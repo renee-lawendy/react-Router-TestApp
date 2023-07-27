794df3f38737c08ed9436a4e391dbfe2eb4ed936
Renee Lawendy <62428850+renee-lawendy@users.noreply.github.com>
2023-07-27 15:44:29 +0000

    resolve build comments

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useLoaderData, useParams } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 export default function ProductDetails() {
  //  use param in case of use Effect
